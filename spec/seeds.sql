223a8773bad4bd230cd7e6a987945a37212d5217
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-04 11:06:47 +0000

    truncated table and inserted values

---
+++ spec/seeds.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE recipes RESTART IDENTITY; -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
+INSERT INTO recipes (the_name, cooking_time_minutes, rating) VALUES ('crumpets', 23, 'not bad' );
+INSERT INTO recipes (the_name, cooking_time_minutes, rating) VALUES ('ice cream', 134, 'amazing');
+INSERT INTO recipes (the_name, cooking_time_minutes, rating) VALUES ('crepe', 32, 'good' );
+INSERT INTO recipes (the_name, cooking_time_minutes, rating) VALUES ('crayons' , 1, 'awful'); -- replace with your own table name.
+
+-- Below this line there should only be `INSERT` statements.
+-- Replace these statements with your own seed data.
+
